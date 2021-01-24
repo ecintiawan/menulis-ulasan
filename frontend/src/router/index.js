306876e3ce15aca9310dfe72adc12c31ed5f6ee4
Christopher Reeve <reeveworkmail@gmail.com>
2021-01-24 20:47:11 +0700

    .

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,17 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: () => import(/* webpackChunkName: "landing" */ "@views/Home.vue")
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: () =>
+            import(/* webpackChunkName: "landing page" */ '@views/Home.vue')
+    }
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 });
 
 export default router;
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Hello menulis ulasan!</h1>
+    <h1>Hello menulis ulasan!</h1>
 </template>
 
 <script>
