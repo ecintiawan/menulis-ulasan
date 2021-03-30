e8c45a7b300deb8c1727de5302b755782cde00ad
Christopher Reeve <reeveworkmail@gmail.com>
2021-03-30 21:05:17 +0700

    .

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,8 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './styles/app.css';
+import App from '@/App.vue';
 import VSkeleton from '@/components/base/VSkeleton.vue';
+import '@/styles/app.css';
+import { createApp } from 'vue';
+
 const app = createApp(App);
 app.component('v-skeleton', VSkeleton);
 app.mount('#app');
--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '../utils/HttpClient';
+import { HttpClient } from '@/utils/HttpClient';
 
 export const api = new HttpClient({
 	baseURL: import.meta.env.VITE_API_URL as string,
